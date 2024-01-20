6520883cf83389d1ae23d54ce0ce3dc439ae28e2
richagautam2002 <153225459+richagautam2002@users.noreply.github.com>
2024-01-20 17:28:02 +0530

    Add files via upload

---
+++ MainPayMoneyDeterminator.java
@@ -0,0 +1,8 @@
+package paymoney.tgt.determinator;
+
+public class MainPayMoneyDeterminator {
+	public static void main(String[] args) {
+        PayMoneyTargetDeterminator determinator = new PayMoneyTargetDeterminator();
+        determinator.determine();
+    }
+}
